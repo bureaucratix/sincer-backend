63790d50b9cc39d6c9a130f0454c44a9620f4cd4
Bureaucratix <bureaucratix@gmail.com>
2020-01-15 22:23:15 -0800

    templating to test 404

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,10 +1,12 @@
 namespace SincerApi.Models
 {
-    public class TaskItem
+  public class TaskItem
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
-       
+        public bool IsRepeat { get; set; }
+        public bool PushNotif { get; set; }
+        public long LastTimestamp { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using SincerApi.Models;
 
@@ -28,6 +21,9 @@ namespace SincerApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TaskContext>(opt =>
+               opt.UseInMemoryDatabase("TaskList"));
+ 
             services.AddControllers();
         }
 
